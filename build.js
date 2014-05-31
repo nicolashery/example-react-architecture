b3782e343c422a781119b2dfd0fc45499f6a7c03
Nicolas Hery <hery.nicolas@gmail.com>
2014-05-31 15:37:20 +0200

    Add command log to build script

--- build.js
+++ build.js
@@ -4,11 +4,15 @@ var _ = require('lodash');
 
 var files = require('./files');
 
+console.log('rm -rf build');
 rm('-rf', 'build');
+console.log('mkdir -p build');
 mkdir('-p', 'build');
 
 _.forEach(files.vendor, function(filePath, fileName) {
+  console.log('cp ' + filePath + ' build/' + fileName);
   cp(filePath, 'build/' + fileName);
 });
 
+console.log('webpack --progress --colors');
 exec('webpack --progress --colors');
